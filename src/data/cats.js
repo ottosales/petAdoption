b911d7e94871dbb35f6087c3d38e1353bd700282
otavio <otavio@leanonsystems.com>
2020-08-11 21:14:38 -0300

    Added a cat and dog database

---
+++ src/data/cats.js
@@ -0,0 +1,42 @@
+export default[
+    {
+        name: 'Archie',
+        breed: 'tuxedo',
+        gender: 'male',
+        age: 7,
+        color: 'black/white',
+        weight: 10,
+        location: 'demon realm',
+        notes: 'Secretly plots world domination'
+    },
+    {
+        name: 'Musashi',
+        breed: 'tabby',
+        gender: 'male',
+        age: 10,
+        color: 'black',
+        weight: 12,
+        location: 'osaka',
+        notes: 'Edgy'
+    },
+    {
+        name: 'Dusk',
+        breed: 'tabby',
+        gender: 'female',
+        age: 2,
+        color: 'gray',
+        weight: 15,
+        location: 'oolacile',
+        notes: 'Purrs a lot; practices black magic'
+    },
+    {
+        name: 'Arthas',
+        breed: 'tuxedo',
+        gender: 'male',
+        age: 2,
+        color: 'blue/gray',
+        weight: 13,
+        location: 'dead lol',
+        notes: "Not prepared"
+    }
+]
---
+++ src/data/dogs.js
@@ -0,0 +1,42 @@
+export default[
+    {
+        name: 'Beatrice',
+        breed: 'mutt',
+        gender: 'female',
+        age: 7,
+        color: 'black/white',
+        weight: 34,
+        location: 'astora',
+        notes: 'Spellcaster, super friendly'
+    },
+    {
+        name: 'Artorias',
+        breed: 'mutt',
+        gender: 'male',
+        age: 12,
+        color: 'blue/black',
+        weight: 37,
+        location: 'the abyss',
+        notes: 'Can traverse the Abyss'
+    },
+    {
+        name: 'Kabosu',
+        breed: 'shiba inu',
+        gender: 'male',
+        age: 6,
+        color: 'yellow/white',
+        weight: 34,
+        location: 'shibuya',
+        notes: 'Funny yellow dog (kinda cringe)'
+    },
+    {
+        name: 'Balltze',
+        breed: 'shiba inu',
+        gender: 'male',
+        age: 4,
+        color: 'yellow/white',
+        weight: 38,
+        location: 'kyoto',
+        notes: "Can't pronunce 'Cheeseburger' correctly"
+    }
+]
